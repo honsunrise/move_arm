b3a7f2e4f97381132324ccd054df6dfac8c3e18b
zhsyourai <zhsyourai@163.com>
2017-06-12 16:00:56 +0800

    add some thing

---
+++ main.py
@@ -0,0 +1,5 @@
+from move import Move
+
+if __name__ == '__main__':
+    move = Move(105, 150)
+    move.to(100, 100, 100)
--- move.py
+++ move.py
@@ -1,25 +1,43 @@
+import serial
+from transform import Transform
 
 
 class Move:
-    def __init__(self):
+    def __init__(self, b, c, port='/dev/ttyUSB0', baudrate=19200):
         self.m1 = 0
         self.m2 = 0
         self.m3 = 0
-        pass
+        self.ser = serial.Serial()
+        self.ser.port = port
+        self.ser.baudrate = baudrate
+        self.transform = Transform(b, c)
 
     def __rotate_m1(self, degrees):
         self.m1 += degrees
-        pass
 
     def __rotate_m2(self, degrees):
         self.m2 += degrees
-        pass
 
     def __rotate_m3(self, degrees):
         self.m3 += degrees
         self.__rotate_m2(degrees)
-        pass
+
+    # #1P600#2P900#8P2500T100\r\n
+    # P 500-2500
+    # T 100-9999
+    def __degrees_to_pos(self, degrees):
+        pos_per_degree = 2000 / 180
+        return 500 + degrees * pos_per_degree
+
+    def __do(self):
+        t = 100
+        param = '#1P%d#2P%d#8P%dT%d\r\n' % \
+                (self.__degrees_to_pos(self.m1), self.__degrees_to_pos(self.m2), self.__degrees_to_pos(self.m3), t)
+        self.ser.write(param)
 
     def to(self, x, y, z):
-        pass
-
+        alpha, beta, gamma = self.transform.i(x, y, z)
+        self.__rotate_m1(gamma)
+        self.__rotate_m3(alpha)
+        self.__rotate_m2(beta)
+        self.__do()
